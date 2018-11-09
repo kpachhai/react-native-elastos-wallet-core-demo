e6922000a2dd973701d08722b8c60842cb9bc9b6
ademcan <ademcan@ademcan.net>
2018-11-09 10:39:00 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import RNElastosMainchain from 'react-native-elastos-mainchain';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+    state={
+        menmonic: ""
+    }
+
+
+    sayHi = () => {
+        console.log("BLABLA.................")
+        console.log(RNElastosMainchain);
+        RNElastosMainchain.generateMnemonic( (err, res) => {
+            console.log(res);
+            this.setState({mnemonic: res})
+        });
+    }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+         <Button title="Generate Mnemonic" onPress={this.sayHi} />
+
+         { this.state.mnemonic == "" ?
+             <View></View>
+             :
+             <View style={{padding:20}}><Text>{this.state.mnemonic}</Text></View>
+         }
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
